5f3700c52dfe5151b7420356151899d8a1c30450
root <root@Rindra.localdomain>
2020-06-26 22:14:26 +0700

    just noting down my screen width and height

--- Koop Frontend/lib/widgets/FabMenu.dart
+++ Koop Frontend/lib/widgets/FabMenu.dart
@@ -5,17 +5,19 @@ import '../screens/FinancePage.dart';
 import '../screens/ProfilePage.dart';
 
 class FabMenu extends StatelessWidget {
-  
+
   @override
   Widget build(BuildContext context) {
     return FabCircularMenu(
       ringDiameter: 300.0,
+      //actual width: 411.4, height: 683.4
       ringWidth: 70.0,
       alignment: Alignment.bottomLeft,
       children: <Widget>[
           Padding(
             padding: const EdgeInsets.all(13.0),
             child: IconButton(icon: Icon(Icons.home), onPressed: () {
+              print(MediaQuery.of(context).size);
                Navigator.of(context).pushReplacementNamed('/');
             }),
           ),
